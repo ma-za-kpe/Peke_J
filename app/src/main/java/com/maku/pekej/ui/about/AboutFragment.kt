89412b7a084804a8c91e316f584c3931f98ac02a
MAZAKPE <makpalyy@gmail.com>
2019-11-04 18:39:58 +0200

    Add the About feature

---
+++ app/src/main/java/com/maku/pekej/ui/about/AboutFragment.kt
@@ -0,0 +1,31 @@
+package com.maku.pekej.ui.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.maku.pekej.R
+import com.maku.pekej.databinding.FragmentAboutBinding
+
+class AboutFragment : Fragment() {
+
+    private lateinit var binding: FragmentAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_about, container, false)
+
+        return binding.root
+    }
+}
