dc6c8b0464464cb761c37babee06864769fd36c4
MAZAKPE <makpalyy@gmail.com>
2019-11-05 13:30:20 +0200

    Add Score package

---
+++ app/src/main/java/com/maku/pekej/ui/score/ScoreViewModel.kt
@@ -0,0 +1,7 @@
+package com.maku.pekej.ui.score
+
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/maku/pekej/ui/score/ScoreViewModelFactory.kt
@@ -0,0 +1,2 @@
+package com.maku.pekej.ui.score
+
